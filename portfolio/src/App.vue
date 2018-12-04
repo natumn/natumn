525f9836f7e70ae220bf71e2915ede003151e679
natumn <natsume.pcuse75@gmail.com>
2018-12-04 17:23:51 +0900

    fix

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="my logo" src="./assets/logo.jpg" width="200" height="200">
+    <HelloWorld/>
   </div>
 </template>
 
